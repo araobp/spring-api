c68fe85b460bad06ca9ed0d64e79f5e629ce02c4
araobp <araobp@users.noreply.github.com>
2022-06-15 19:19:56 +0900

    Update BoxRepository.java

--- src/main/java/araobp/domain/repository/BoxRepository.java
+++ src/main/java/araobp/domain/repository/BoxRepository.java
@@ -12,9 +12,5 @@ public interface BoxRepository extends CrudRepository<Box__c, Double> {
 	@Modifying
 	@Query("UPDATE box__c SET move__c = :move__c WHERE id__c = :id__c")
 	public Integer updateBox(@Param("id__c") Double id__c, @Param("move__c") Boolean move__c);
-	
-	@Modifying
-	@Query("UPDATE box__c SET count__c = :count__c WHERE id__c = :id__c")
-	public Integer updateCount(@Param("id__c") Double id__c, @Param("count__c") Double count__c);
-	
+		
 }
